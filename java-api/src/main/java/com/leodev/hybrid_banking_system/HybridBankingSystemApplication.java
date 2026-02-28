564bb37134dee376e03e17b79e8e610874a495ee
leonardoteles31 <leo.teleso.10@hotmail.com>
2026-02-28 03:18:15 -0300

    Start project commit :rocket:

---
+++ java-api/src/main/java/com/leodev/hybrid_banking_system/HybridBankingSystemApplication.java
@@ -0,0 +1,13 @@
+package com.leodev.hybrid_banking_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HybridBankingSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HybridBankingSystemApplication.class, args);
+	}
+
+}
---
+++ java-api/src/test/java/com/leodev/hybrid_banking_system/HybridBankingSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.leodev.hybrid_banking_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HybridBankingSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
